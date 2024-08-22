45bbb0fcd2d5e13a556832317e8422371e7df2a8
Olex Tkachuk <sasha.tkachuk@gmail.com>
2024-08-22 23:42:14 +1000

    Less data

--- src/main.rs
+++ src/main.rs
@@ -108,7 +108,7 @@ fn main() {
         .open(&filename)
         .expect("Unable to create output file");
     let mut count = 0;
-    while count < 60 {
+    while count < 10 {
         println!("Waiting for camera request execution");
         let mut req = rx.recv_timeout(Duration::from_secs(2)).expect("Camera request failed");
 
